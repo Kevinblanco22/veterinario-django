34cb99702071886de06b30d343ab5e32aa1d3c38
kevin blanco <kevinblanco22@hotmail.com>
2025-11-20 15:34:45 -0500

    ruta

--- vetproject/urls.py
+++ vetproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from veterinariaproject.clinic.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='home'),
     path('clinic/', include('clinic.urls')),
 ]
