4fb326899e3b64d89c99df8cf1c2d1aac2aff327
kevin blanco <kevinblanco22@hotmail.com>
2025-11-20 15:45:42 -0500

    corregida url

--- vetproject/urls.py
+++ vetproject/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from veterinariaproject.clinic.views import IndexView
+from clinic.views import IndexView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
