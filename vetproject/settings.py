872a5e1e88736fc1cff15577f2acb81756b304de
kevin blanco <kevinblanco22@hotmail.com>
2025-11-20 15:17:30 -0500

    settings

--- vetproject/settings.py
+++ vetproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-o)ki+@*jcd4-h@w$7e$0l9_961q0b2d6w!p3z92x2f%c%jg41$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'clinic/static',  # Esto agrega la ruta de 'clinic/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
